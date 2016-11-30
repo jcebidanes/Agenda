79f62ea894ef058bc88080b92bd5fd6aabaefe8d
Jhonnatan Cebidanes <jcebidanes@gmail.com>
2016-11-30 17:11:09 -0200

    Consumindo media server!

--- app/src/main/java/com/cebidanes/agenda/AlunoConverter.java
+++ app/src/main/java/com/cebidanes/agenda/AlunoConverter.java
@@ -17,14 +17,14 @@ public class AlunoConverter {
         JSONStringer js = new JSONStringer();
 
         try {
-            js.object().key("list").array().object().key("aluno").array();
+            js.array();
             for (Aluno aluno : alunos) {
                 js.object();
                 js.key("nome").value(aluno.getNome());
                 js.key("nota").value(aluno.getNota());
                 js.endObject();
             }
-            js.endArray().endObject().endArray().endObject();
+            js.endArray();
 
         } catch (JSONException e) {
             e.printStackTrace();
